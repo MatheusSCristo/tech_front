b06695c649d06790f3be53b9e39d1d7f3e6324c6
Matheus <cristo.matheus@outlook.com>
2024-08-21 07:55:59 -0300

    fix: Add handle to failed token validation fetch

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest,res: NextResponse) {
   const publicPaths = ["/_next", "/favicon.ico", "/static"];
   if (publicPaths.some((path) => req.url.startsWith(path))) {
     return NextResponse.next();
@@ -12,21 +12,24 @@ export async function middleware(req: NextRequest) {
   }
 
   if (token) {
-    const response = await fetch(
-      `http://localhost:8080/auth/validate/${token.value}`
-    );
+    try {
+      const response = await fetch(`http://localhost:8080/auth/validate/${token.value}`);
 
-    if (response.status !== 200 && !req.url.includes("/auth")) {
-      return NextResponse.redirect(new URL("/auth/login", req.url));
-    }
+      if (response.status !== 200 && !req.url.includes("/auth")) {
+        return NextResponse.redirect(new URL("/auth/login", req.url));
+      }
 
-    if (response.status === 200 && req.url.includes("/auth")) {
-      console.log("redirecting to /");
-      return NextResponse.redirect(new URL("/", req.url));
+      if (response.status === 200 && req.url.includes("/auth")) {
+        console.log("redirecting to /");
+        return NextResponse.redirect(new URL("/", req.url));
+      }
+      return NextResponse.next();
+    } catch (error) {
+      const response= NextResponse.redirect(new URL("/auth/login", req.url));
+      response.cookies.delete("access_token");
+      return response;
     }
   }
-
-  return NextResponse.next();
 }
 
 export const config = {
