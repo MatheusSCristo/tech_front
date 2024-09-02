d6abf77c78ce82094ac82d3cce3a0007faa03760
Matheus <cristo.matheus@outlook.com>
2024-09-02 17:43:37 -0300

    feat: Add mark all as finished button

--- src/app/(pages)/(nav)/semesters/Semester/index.tsx
+++ src/app/(pages)/(nav)/semesters/Semester/index.tsx
@@ -2,7 +2,13 @@ import { SemesterUserType } from "@/types/semester";
 import { SemesterSubjectType } from "@/types/semesterSubject";
 import { Draggable, Droppable } from "@hello-pangea/dnd";
 import { useState } from "react";
+import ErrorPopUp from "../ErrorPopUp";
 import SubjectModal from "../SubjectModal";
+
+type ErrorType = {
+  text: string;
+  status: boolean;
+};
 
 const Semester = ({
   semester,
@@ -15,35 +21,100 @@ const Semester = ({
   setSemesters: React.Dispatch<React.SetStateAction<SemesterUserType[]>>;
   semesters: SemesterUserType[];
 }) => {
+  const [error, setError] = useState({} as ErrorType);
+
+  const checkIfPreRequisitesAreFinished = (subject: SemesterSubjectType) => {
+    const preRequisites = subject.subject.pre_requisites;
+    const preRequisitesNotFinished = preRequisites.filter((prerequisite) =>
+      semesters.some((semester) =>
+        semester.subjects.some(
+          (semesterSubject) =>
+            semesterSubject.subject.id === prerequisite.id &&
+            !semesterSubject.finished
+        )
+      )
+    );
+    return preRequisitesNotFinished;
+  };
+
+  const handleFinishAllSubjects = () => {
+    const newSemesterSubjects = semester.subjects.map((semesterSubject) => {
+      if (checkIfPreRequisitesAreFinished(semesterSubject).length) {
+        const subjectThatHasPrerequistesNotFinished = semester.subjects
+          .filter((semesterSubject) =>
+            checkIfPreRequisitesAreFinished(semesterSubject).length
+          )
+          .flatMap((subject) => subject);
+
+          const subjectThatHasPrerequistesNotFinishedText=subjectThatHasPrerequistesNotFinished.map((subject)=>subject.subject.name).join(", ");
+
+        setError({
+          text: `Não é possível concluir os componentes ${subjectThatHasPrerequistesNotFinishedText}. Pois possuem pre requisitos  não concluidos.`,
+          status: true,
+        });
+        return semesterSubject;
+      }
+      return {
+        ...semesterSubject,
+        finished: true,
+      };
+    });
+    setSemesters((prevState) =>
+      prevState.map((oldSemester) => {
+        if (oldSemester.id === semester.id) {
+          return { ...oldSemester, subjects: newSemesterSubjects };
+        } else {
+          return oldSemester;
+        }
+      })
+    );
+  };
+
   return (
-    <Droppable
-      droppableId={`semester-${semester.id}`}
-      type="list"
-      direction="horizontal"
-    >
-      {(provided) => (
-        <div
-          ref={provided.innerRef}
-          {...provided.droppableProps}
-          className="flex items-center border-b-[1px] border-black pb-5"
-        >
-          <span className="font-bold text-xl">{semester.semester}°</span>
-          <div className="flex flex-wrap gap-10 justify-center w-full">
-            {semester.subjects.map((subject, index) => (
-              <Subject
-                subject={subject}
-                index={index}
-                key={subject.id}
-                setSemesters={setSemesters}
-                semester={semester}
-                semesters={semesters}
-              />
-            ))}
+    <>
+      <Droppable
+        droppableId={`semester-${semester.id}`}
+        type="list"
+        direction="horizontal"
+      >
+        {(provided) => (
+          <div className="flex flex-col border-b-[1px] border-black pb-5 gap-2">
+            <div
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              className="flex items-center"
+            >
+              <span className="font-bold text-xl">{semester.semester}°</span>
+              <div className="flex flex-wrap gap-10 justify-center w-full">
+                {semester.subjects.map((subject, index) => (
+                  <Subject
+                    subject={subject}
+                    index={index}
+                    key={subject.id}
+                    setSemesters={setSemesters}
+                    semester={semester}
+                    semesters={semesters}
+                  />
+                ))}
+              </div>
+              {provided.placeholder}
+            </div>
+            <button
+              className="self-end border-black border px-1 rounded"
+              onClick={handleFinishAllSubjects}
+            >
+              Marcar todos como concluidos
+            </button>
           </div>
-          {provided.placeholder}
-        </div>
+        )}
+      </Droppable>
+      {error.status && (
+        <ErrorPopUp
+          error={error.text}
+          handleClosePopUp={() => setError({} as ErrorType)}
+        />
       )}
-    </Droppable>
+    </>
   );
 };
 
@@ -54,13 +125,13 @@ const Subject = ({
   index,
   setSemesters,
   semester,
-  semesters
+  semesters,
 }: {
   subject: SemesterSubjectType;
   index: number;
   setSemesters: React.Dispatch<React.SetStateAction<SemesterUserType[]>>;
-  semester:SemesterUserType;
-  semesters:SemesterUserType[];
+  semester: SemesterUserType;
+  semesters: SemesterUserType[];
 }) => {
   const [openSubjectPopUp, setOpenSubjectPopUp] = useState(false);
 
@@ -70,11 +141,13 @@ const Subject = ({
         {(provided) => (
           <div
             onClick={() => setOpenSubjectPopUp(true)}
-            onDrag={()=>setOpenSubjectPopUp(false)}
+            onDrag={() => setOpenSubjectPopUp(false)}
             ref={provided.innerRef}
             {...provided.draggableProps}
             {...provided.dragHandleProps}
-            className={`${subject.finished ? "opacity-[50%]":"opacity-1"} overflow-hidden bg-mandatoryBlue border border-black h-[200px] w-[10%] flex items-center justify-center rounded p-3`} 
+            className={`${
+              subject.finished ? "opacity-[50%]" : "opacity-1"
+            } overflow-hidden bg-mandatoryBlue border border-black h-[200px] w-[10%] flex items-center justify-center rounded p-3`}
           >
             <span className="text-center text-wrap truncate">
               {subject.subject.name}
