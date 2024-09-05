4906030739966a5ba44ca179dd0bc309f921222e
Matheus <cristo.matheus@outlook.com>
2024-09-05 16:12:06 -0300

    fix: Fixed UserContext user local storage retrieve

--- src/app/context/UserContext.tsx
+++ src/app/context/UserContext.tsx
@@ -14,8 +14,7 @@ const UserProvider = ({ children }: { children: React.ReactNode }) => {
   const { setSemesters } = useContext(SemesterContext);
   useEffect(() => {
     const user = localStorage.getItem("user");
-    
-    if (user) {
+    if (user !="null" && user) {
       const newUser:UserType = JSON.parse(user);
       setUser(newUser);
       setSemesters(newUser.semesters.sort((a,b)=>a.semester-b.semester));
